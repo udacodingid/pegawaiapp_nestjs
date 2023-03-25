8c23b5fa85ea552f0743757f0ec1f367af262b02
Udacoding Indonesia <78523253+udacodingid@users.noreply.github.com>
2023-03-25 11:46:31 +0700

    agar end point bisa d panggil

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true }); // agar end point bisa d panggil
   await app.listen(3000);
+  app.enableCors();
 }
+// async function bootstrap() {
+//   // const app = await NestFactory.create(AppModule);
+//   const app = await NestFactory.create(AppModule, {cors: {
+//     origin: true,
+//     preflightContinue: false,
+//   }});
+//   await app.listen(3000);
+// }
 bootstrap();
